a9fde76a8d6626203e3dae949ab326bd10de794b
mrjmac <40571030+mrjmac@users.noreply.github.com>
2022-11-04 15:37:47 -0500

    "winter storm"

--- TeamCode/src/main/java/OceanCrashLinearOpMode/TestAny.java
+++ TeamCode/src/main/java/OceanCrashLinearOpMode/TestAny.java
@@ -87,18 +87,23 @@ public class TestAny extends LinearOpMode {
         drive.setPoseEstimate(startingPose);
 
         TrajectorySequence traj1 = drive.trajectorySequenceBuilder(startingPose)
-                //.addTemporalMarker(.25, () -> targetPos = 700)
-                .lineToLinearHeading(new Pose2d(-34, 36, 0))
-                .lineToLinearHeading(new Pose2d(-22, 34.541, Math.toRadians(-45)))
+                .addTemporalMarker(.25, () -> targetPos = 1500)
+                .lineToLinearHeading(new Pose2d(-34, 36, 0), SampleMecanumDrive.getVelocityConstraint(40, Math.toRadians(80), DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .lineToLinearHeading(new Pose2d(-20.8, 35.091, Math.toRadians(-45)))
                 .build();
 
-        Trajectory traj2 = drive.trajectoryBuilder(traj1.end().plus(new Pose2d(0, 0, Math.toRadians(135))))
+        TrajectorySequence turn135 = drive.trajectorySequenceBuilder(traj1.end())
+                .addDisplacementMarker(()-> targetPos = 700)
+                .turn(Math.toRadians(135))
+                .build();
+
+        Trajectory traj2 = drive.trajectoryBuilder(turn135.end())
                 //.lineToLinearHeading(new Pose2d(-29.5, 34.5, Math.toRadians(90)))
-                .lineToLinearHeading(new Pose2d(-23.6, 54.5, Math.toRadians(90)), SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, Math.toRadians(80), DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .lineToLinearHeading(new Pose2d(-22.35, 54, Math.toRadians(90)), SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, Math.toRadians(80), DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
                 .build();
 
         Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
-                .lineToLinearHeading(new Pose2d(-22.471, 13.6, Math.toRadians(90)), SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, Math.toRadians(80), DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+                .lineToLinearHeading(new Pose2d(-19.5, 12.6, Math.toRadians(90)), SampleMecanumDrive.getVelocityConstraint(40, Math.toRadians(80), DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
                 .build();
 
         Trajectory traj4 = drive.trajectoryBuilder(traj3.end())
@@ -107,62 +112,16 @@ public class TestAny extends LinearOpMode {
 
 
 
-
-
-
-    /*
-        TrajectorySequence one = drive.trajectorySequenceBuilder(startingPose)
-
-                .addTemporalMarker(.25, () -> targetPos = 700)
-                .lineToLinearHeading(new Pose2d(-36, 36, 0))
-                .lineToLinearHeading(new Pose2d(-23.5, 34.5, Math.toRadians(-45)))
-                .lineToLinearHeading(new Pose2d(-29.5, 34.5, Math.toRadians(-45)))
-                .lineToLinearHeading(new Pose2d(-27, 53.25, Math.toRadians(90)), SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, Math.toRadians(80), DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
-                .lineToLinearHeading(new Pose2d(-27.5, 34.5, Math.toRadians(-45)))
-                .lineToLinearHeading(new Pose2d(-23.5, 34.5, Math.toRadians(-45)))
+/*
+        TrajectorySequence turn45 = drive.trajectorySequenceBuilder(traj3.end())
+                .turn(Math.toRadians(45))
                 .build();
 
-     */
+        TrajectorySequence turnNeg45 = drive.trajectorySequenceBuilder(turn45.end())
+                .turn(Math.toRadians(-45))
+                .build();
 
-/*
-        TrajectorySequence test = drive.trajectorySequenceBuilder(startingPose)
-                .addTemporalMarker(.25, () -> lift.setLiftPower(-.4))
-                .addTemporalMarker(.75, () -> lift.setLiftPower(0))
-                .lineToLinearHeading(new Pose2d(-36, 36, 0))
-                /*
-                .lineToLinearHeading(new Pose2d(-36, 36, Math.toRadians(0)))
-                .waitSeconds(2)
-                //.UNSTABLE_addTemporalMarkerOffset(0, () -> lift.setLiftPos(1500))
-                .lineToLinearHeading(new Pose2d(-24, 35, Math.toRadians(-47)))
-                .waitSeconds(3)
-                //drop && put lift down
-                .UNSTABLE_addTemporalMarkerOffset(0, () -> lift.extendFourBar())
-                .UNSTABLE_addTemporalMarkerOffset(-0.5, () -> lift.setLiftPos(700))
-                .turn(Math.toRadians(137))
-                .waitSeconds(2)
-                .lineToLinearHeading(new Pose2d(-27, 57.75, Math.toRadians(90)), SampleMecanumDrive.getVelocityConstraint(20.0, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(50))
-                .UNSTABLE_addTemporalMarkerOffset(-0.5, () -> lift.release())
-                .UNSTABLE_addTemporalMarkerOffset(4, () -> lift.resetLift(.3, 600))
-                .UNSTABLE_addTemporalMarkerOffset(5, () -> lift.grab())
-                .UNSTABLE_addTemporalMarkerOffset(5.6, () -> lift.setLiftPos(900))
-                .waitSeconds(6)
-                .lineToLinearHeading(new Pose2d(-24, 17, Math.toRadians(90)))
-                .waitSeconds(3)
-                .turn(Math.toRadians(-47))
-                .waitSeconds(2)
-
-                /*
-                //.lineToLinearHeading(new Pose2d(-23, 31.5, Math.toRadians(90)))
-                .waitSeconds(2)
-                .lineToLinearHeading(new Pose2d(-24, 68, Math.toRadians(90)))
-                .waitSeconds(2)
-                .turn(-45)
-                .waitSeconds(2)
-                .lineToLinearHeading(new Pose2d(-23, 31.5, Math.toRadians(-45)))
-                .waitSeconds(2)
-
-                 */
-                //.build();
+         */
 
         while(!isStarted()){
             pos = vision.getPark();
@@ -212,14 +171,17 @@ public class TestAny extends LinearOpMode {
                         if (first) {
                             auto = State.turn135;
                             first = false;
-                            drive.turnAsync(Math.toRadians(135));
+                            drive.followTrajectorySequenceAsync(turn135);
+                            //drive.turnAsync(Math.toRadians(135));
                         } else {
                             if (cycle != cycleTarget) {
                                 if (gamepad1.dpad_up && button.milliseconds() > 200) {
                                     auto = State.turn45;
                                     if (turnCount % 2 == 1)
+                                        //drive.followTrajectorySequenceAsync(turnNeg45);
                                         drive.turnAsync(Math.toRadians(-45));
                                     else
+                                        //drive.followTrajectorySequenceAsync(turn45);
                                         drive.turnAsync(Math.toRadians(45));
                                     cycle++;
                                     button.reset();
@@ -272,8 +234,10 @@ public class TestAny extends LinearOpMode {
 
                             auto = State.turn45;
                             if (turnCount % 2 == 1)
+                                //drive.followTrajectorySequenceAsync(turnNeg45);
                                 drive.turnAsync(Math.toRadians(-45));
                             else
+                                //drive.followTrajectorySequenceAsync(turn45);
                                 drive.turnAsync(Math.toRadians(45));
                             button.reset();
                         }
